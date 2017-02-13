929fa5370c4e7fec090bf82d365456413d83ea72
Toni Hernandez <woulf_alpha@hotmail.com>
2017-02-13 16:35:06 +0100

    ADD

--- application/views/login.php
+++ application/views/login.php
@@ -15,6 +15,7 @@
             </div>
 
 
+
     </div>
     <div class="mdl-card__actions mdl-card--border">
         <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" type="submit">Iniciar Sesion</button>
