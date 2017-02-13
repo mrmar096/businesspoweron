b33144b5527f66d5eaeb5c26ad14c3aa77a53aba
Woulf_Alpha <woulf_alpha@hotmail.com>
2017-02-13 14:50:15 +0100

    ajlkfjaslkfjasldkfjlask

--- application/views/commons/header.php
+++ application/views/commons/header.php
@@ -7,12 +7,13 @@
     <!--Material Design Fonts-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"type="text/css" rel="stylesheet">
     <link href="<?=base_url('resources/css/style.css')?>" type="text/css" rel="stylesheet">
-    <link rel="stylesheet" href="<?=base_url('/bower_components/material-design-lite/material.min.css')?>" type="text/css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-light_blue.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<!--Para Cargar Vistas por ejemplo un nav se haria asi
-<?php// $this->load->view('commons/nav');
-?>
--->
 
+<?php
+    if ($this->session->get_userdata()){
+        $this->load->view('commons/nav');
+    }
+?>
---
+++ application/views/commons/nav.php
@@ -0,0 +1,20 @@
+
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+
+    <div
+        class="android-header mdl-layout__header mdl-layout__header--waterfall">
+        <div class="mdl-layout__header-row">
+				<span class="android-title mdl-layout-title title-font"> <img
+                        class="android-logo-image" src="<?=base_url('resources/img/logo_blank.png');?>"> <span
+                        class="title-font"></span>
+				</span>
+            <!-- Add spacer, to align navigation to the right in desktop -->
+            <div class="android-header-spacer mdl-layout-spacer"></div>
+
+            <!-- Navigation -->
+            <div class="android-navigation-container">
+                <a class="mdl-navigation__link" href=""></a>
+            </div>
+        </div>
+    </div>
+
