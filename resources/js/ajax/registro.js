c25214c02cf3de81df35c46a806f511c410ce30b
mrmar096 <mrmarapps@gmail.com>
2017-02-13 16:33:14 +0100

    Update Ajax Form

--- resources/js/ajax/registro.js
+++ resources/js/ajax/registro.js
@@ -1,17 +1,16 @@
 /**
  * Created by Mario on 09/02/2017.
  */
-
-
 $(function() {
-    //$("#error-block").hide();
+	$("#error-block").hide();
 	$('form').on('submit', function(e) {
 		e.preventDefault();
 		var url=this.action;
 		console.log(url);
-        var form=$(this);
-		var btn= $(this).children("button[type='submit']");
+		var form=$(this);
+		var btn=$(this).find("button[type='submit']");
 		var btnvalue=btn.text();
+
 		$.ajax({
 			type:'POST',
 			url:url,
@@ -21,13 +20,13 @@ $(function() {
 				btn.text("Enviando...");
 			},
 			success:function (response) {
-                var data=JSON.parse(response);
+				var data=JSON.parse(response);
 				console.log(data);
 				if(data.status==1){
-                    console.log(response);
+					console.log(response);
 				}else{
-                    console.log(data.mensaje);
-                    var dismiss='<button type="button" class="close" data-dismiss="alert">×</button>';
+					console.log(data.mensaje);
+					var dismiss='<button type="button" class="close" data-dismiss="alert">×</button>';
 					$("#error-block").html(dismiss+" "+data.mensaje).slideDown();
 				}
 				btn.text(btnvalue);
